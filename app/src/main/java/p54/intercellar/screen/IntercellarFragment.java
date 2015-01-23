4990e156aeaebe3112a97592ed047cccc5dbd652
sreiss <simon.reiss57@gmail.com>
2015-01-23 14:43:55 +0100

    Reflexive constructors

--- app/src/main/java/p54/intercellar/screen/IntercellarFragment.java
+++ app/src/main/java/p54/intercellar/screen/IntercellarFragment.java
@@ -6,7 +6,7 @@ import android.app.Fragment;
  * Created by sreiss on 23/01/15.
  */
 public abstract class IntercellarFragment extends Fragment {
-    //private IntercellarController controller;
+    //private InterCellarController controller;
 
 
 }
